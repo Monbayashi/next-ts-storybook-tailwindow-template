38f16830e04e3dde289ed341cee0aa4ca98a0fa3
monbayashi <monbayashi@saterise.com.jp>
2022-06-14 13:23:12 +0900

    Storybook 初期ページを表示してみる

---
+++ src/stories/pages/index.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import Home from '../../pages/index';
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+} as ComponentMeta<typeof Home>;
+
+export const HomePage: ComponentStoryObj<typeof Home> = {};
